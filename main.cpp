66b3cfdec01d542071aa671b2aa708653f80935d
Aditya C <87110942+AdC-003@users.noreply.github.com>
2021-11-01 20:12:47 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,64 @@
+/*    Single Matrix Generator    */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const ll NN = 1000;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+ll gr(ll r)
+{
+   uniform_int_distribution<ll> uid(0,r-1);
+   ll val = uid(rng);
+   return val+1;
+}
+
+ll glr(ll l, ll r)
+{
+    ll dif = r-l+1;
+    ll x = gr(dif) + l-1;
+    return x;
+}
+
+#define cout outputFile[tc]
+
+int main()
+{
+    string file[5] = {"T1.txt", "T2.txt", "T3.txt", "T4.txt", "T5.txt"};
+    ofstream outputFile[5];
+    for (ll i=0; i<5; i++) outputFile[i].open(file[i].c_str());
+
+    for(ll tc=0; tc<5; tc++)
+    {
+        ll t = 1;
+        cout<<t<<endl;
+        while(t--)
+        {
+            ll m = gr(NN);
+            ll n = gr(NN);
+            ll p = gr(NN);
+            cout<<m<<' '<<n<<' '<<p<<endl;
+
+            for(ll i=0; i<m; i++)
+            {
+                for(ll j=0; j<n; j++)
+                {
+                    cout<<gr(1e4)<<' ';
+                }
+                cout<<endl;
+            }
+            for(ll i=0; i<n; i++)
+            {
+                for(ll j=0; j<p; j++)
+                {
+                    cout<<gr(1e4)<<' ';
+                }
+                cout<<endl;
+            }
+        }
+    }
+    return 0;
+}
+
