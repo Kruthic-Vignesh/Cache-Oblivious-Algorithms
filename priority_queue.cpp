1aaa3be3550c2fe4c6a295dc9cc08f5f1b7eea9c
Kruthic Vignesh M <80332221+Kruthic-Vignesh@users.noreply.github.com>
2021-12-06 17:38:04 +0530

    Update priority_queue.cpp

--- priority_queue.cpp
+++ priority_queue.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 typedef long long ll;
 typedef vector<ll>* vec_ptr;
+typedef long long datatype;
 
 const ll c = 12;
 
@@ -19,7 +20,7 @@ class lvl
 public:
 	vector<ll> up_bf;
 	vector<bf> down_bf;
-	ll fir;
+	ll fir, down_bf_cnt;
 	/*
 	up_sz   : max size of up buffer (X^3/2)
 	down_sz : max size of down buffer (except first one) (2*X-1)
@@ -38,11 +39,13 @@ public:
 	ll del_min();
 	void push(ll, vec_ptr);
 	void pull(ll, vec_ptr);
+	void desc_sort(vector<ll>&);
+	void sort(vector<ll>&);
 };
 
 void priority_q::push(ll lno, vec_ptr a) //level[lno] access, push X elements to level lno
 {
-//sort(a);
+sort(a);
 	ll ind = 0;
 	for(ll i = level[lno].fir; i != -1; i = level[lno].down_bf[i].next)
 	{
@@ -52,13 +55,13 @@ void priority_q::push(ll lno, vec_ptr a) //level[lno] access, push X elements to
 			ind++;
 			if((*(level[lno].down_bf[i].st)).size() == level[lno].down_sz+1)
 			{
-//split();
+//split(); inc down_bf_cnt
 //if(level[lno].down_bf[i].size() == level[lno].down_bf[i].down_cnt+1)
 //{
 //push final dbf into upbf
 //	if(level[lno].up_bf.size() > level[lno].up_sz)
 //     recursive push(X^3/2 elements)
-//clear}
+//}
 			}
 		}
 	}
@@ -78,17 +81,50 @@ void priority_q::pull(ll lno, vec_ptr a)	//pull elements from level lno
 {
 	ll ind = level[lno].fir;
 	ll min_sz = (level[lno].down_sz + 1)/2;
-	if((*(level[lno].down_bf[ind].st)).size() >= min_sz)
+	if(level[lno].down_bf_cnt > 0 && (*(level[lno].down_bf[ind].st)).size() >= min_sz)
 	{
-//sort the ind buffer
-//take away least min_sz elements & put to a
-//leave remaining in the buffer
+desc_sort((level[lno].down_bf[ind].st));
+		//take away least min_sz elements & put to a
+		//leave remaining in the buffer
+		while((*a).size() < min_sz)
+		{
+			(*a).push_back( ( *( level[lno].down_bf[ind].st ) ) .back() );
+			( *( level[lno].down_bf[ind].st ) ).pop_back();
+			level[lno].cnt--;
+		}
+		if(( *( level[lno].down_bf[ind].st ) ).size() == 0)
+		{
+			level[lno].fir = level[lno].down_bf[ind].next;
+			level[lno].down_bf_cnt--;
+		}
 	}
-	else if(level[lno].down_bf.size() > 1)
+	else if(level[lno].down_bf_cnt > 1)
 	{
-//pull first buffer fully
-//sort second buffer
-//take reqd no of elements from second buffer
+		//pull first buffer fully
+		while( (*(level[lno].down_bf[ind].st)).size() > 0)
+		{
+			(*a).push_back( ( *( level[lno].down_bf[ind].st ) ) .back() );
+			( *( level[lno].down_bf[ind].st ) ).pop_back();
+			level[lno].cnt--;
+		}
+		/* Removing the first down_bf from the list */
+		level[lno].fir = level[lno].down_bf[ind].next;
+		level[lno].down_bf_cnt--;
+		ind = level[lno].fir; //ind points to the current first down_bf
+	//sort second buffer
+desc_sort((level[lno].down_bf[ind].st));
+		//take reqd no of elements from second buffer
+		while((*a).size() < min_sz)
+		{
+			(*a).push_back( ( *( level[lno].down_bf[ind].st ) ) .back() );
+			( *( level[lno].down_bf[ind].st ) ).pop_back();
+			level[lno].cnt--;
+		}
+		if(( *( level[lno].down_bf[ind].st ) ).size() == 0)
+		{
+			level[lno].fir = level[lno].down_bf[ind].next;
+			level[lno].down_bf_cnt--;
+		}
 	}
 	else
 	{
@@ -97,8 +133,9 @@ void priority_q::pull(ll lno, vec_ptr a)	//pull elements from level lno
 			(*a).push_back(tt);
 		}
 		(*(level[lno].down_bf[ind].st)).clear();
-
-		//if(lno == last level)
+		level[lno].down_bf_cnt = 0;
+		level[lno].cnt = 0;
+		if(lno == level.size()-1)
 		{
 			//take elements from upbuffer
 			//sort and take enough to fill downbuffer
@@ -106,8 +143,8 @@ void priority_q::pull(ll lno, vec_ptr a)	//pull elements from level lno
 			//return
 		}
 		vec_ptr b;
-		pull(lno,b);
-		if(level[lno].cnt < min_sz)
+		pull(lno+1, b);
+		/*if(level[lno].cnt < min_sz)
 		{
 			//take all values from this level
 			//put into prev level
@@ -115,35 +152,38 @@ void priority_q::pull(ll lno, vec_ptr a)	//pull elements from level lno
 			//return
 		}
 		else
-		{
+		{*/
 			ll u_sz = level[lno].up_bf.size();
 			for(ll tt : level[lno].up_bf)
 			{
 				(*b).push_back(tt);
 			}
 			level[lno].up_bf.clear();
-//sort(*b);
-			ll how_many_removed = 0;
-			for(ll i=0; (*a).size() < min_sz; i++)	//taking least [X]-Y keys and putting into a
+//sort b and refill the first U elements into the up_bf;
+sort(b);
+			ll how_many_pulled = 0;
+			for(ll i=0; (*a).size() < min_sz && i < b->size(); i++)	//taking least [X]-Y keys and putting into a
 			{
 				(*a).push_back(b->at(i));
-				how_many_removed++;
+				how_many_pulled++;
 			}
 
 			ll count = 0;
-			for(ll i=ll((*b).size())-1; i>=how_many_removed && count < u_sz; i--, count++)		//refilling upbuffer
+			for(ll i=ll((*b).size())-1; i>=how_many_pulled && count < u_sz; i--, count++)		//refilling upbuffer
 			{
 				level[lno].up_bf.push_back(b->at(i));
 				(*b).pop_back();
 			}
 
-			if(((*b).size() - how_many_removed) == 0)
+			if(((*b).size() - how_many_pulled) == 0)
 				return;
-
-			ll rem = ((*b).size() - how_many_removed) % min_sz;		//No of elements to be put in down_bf[0]
+			/* Refilling the first down_buffer of lno, size = rem & updating cnt of lno*/
+			level[lno].cnt = (*b).size() - how_many_pulled;
+			ll rem = ((*b).size() - how_many_pulled) % min_sz;		//No of elements to be put in down_bf[0]
 			if(!rem)
 				rem = min_sz;
-			ll ar_pos = how_many_removed;
+			level[lno].down_bf_cnt++;
+			ll ar_pos = how_many_pulled;
 			ll curr_bf = 0;
 			level[lno].fir = curr_bf;
 			for(ll i=0; i<rem; i++)
@@ -151,19 +191,21 @@ void priority_q::pull(ll lno, vec_ptr a)	//pull elements from level lno
 				(*(level[lno].down_bf[curr_bf].st)).push_back(b->at(ar_pos));
 				ar_pos++;
 			}
-
-			ll how_many_bfs = ((*b).size()-how_many_removed-rem) / min_sz;
+			/* Refilling the remaining down_buffers of lno */
+			ll how_many_bfs = ((*b).size()-how_many_pulled-rem) / min_sz;
+			level[lno].down_bf_cnt += how_many_bfs; //no of down_bfs = first + how_many_bfs
 			for(ll i=1; i<=how_many_bfs; i++)
 			{
-				level[lno].down_bf[curr_bf].next = i;
+				level[lno].down_bf[curr_bf].next = i; //next of previous down_bf = i
 				curr_bf = i;
 				for(ll j=0; j<min_sz; j++)
 				{
 					(*(level[lno].down_bf[curr_bf].st)).push_back(b->at(ar_pos));
+					ar_pos++;
 				}
-				level[lno].down_bf[curr_bf].next = -1;
+				level[lno].down_bf[curr_bf].next = -1;//next of the new down_bf = -1
 			}
-		}
+		// }
 	}
 }
 
