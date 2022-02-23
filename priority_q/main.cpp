320301c9499f7ea91d18aa54ce666110f6de6e4a
Kruthic Vignesh M <80332221+Kruthic-Vignesh@users.noreply.github.com>
2022-02-23 11:37:48 +0530

    Update main.cpp

--- priority_q/main.cpp
+++ priority_q/main.cpp
@@ -1,4 +1,3 @@
-#include<bits/stdc++.h>
 #include "classes.h"
 #include "PQ_func.h"
 #include "push.h"
@@ -6,5 +5,44 @@
 
 int main()
 {
+    priority_q a;
+    for(int i = 1; i <= 20; i++) 
+    {
+        //cout<<"\nInserting "<<i<<"...\n";
+        a.insert(i);
+        //a.traverse(0);
+    }
+cout<<"yes"<<endl;
+    for(int i = 1; i <= 10; i++)
+    {
+        cout<<endl<<a.del_min()<<" s"<<endl;
+        // a.traverse(0);
+    }
+
+    for(int i = 30; i <= 37; i++) 
+    {
+        cout<<"\nInserting "<<i<<"...\n";
+        a.insert(i);
+        a.traverse(0);
+        cout<<"\ndel_bf "<<i<<"...\n";
+    for(ll x: a.del_bf)
+    {
+        cout<<x<<" ";
+    }
+    cout<<endl;
+    }
+
+    for(int i = 1; i <= 17; i++)
+    {
+        cout<<a.del_min()<<" s"<<endl;
+    }
+    cout<<endl;
+    // for(int i = 1; i <= 4; i++) cout<<"s "<<a.del_min()<<" ";
+    // cout<<endl;
+    /*for(int i = 30; i >= 20; i--)
+    {
+        a.insert(i);
+    }
+    for(int i = 1; i <= 17; i++) a.del_min();*/
     return 0;
 }
