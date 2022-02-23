9d735224b197b02e58f91a0ad4176f72679abfe3
Kruthic Vignesh M <80332221+Kruthic-Vignesh@users.noreply.github.com>
2022-02-23 11:37:35 +0530

    Update classes.h

--- priority_q/classes.h
+++ priority_q/classes.h
@@ -1,14 +1,37 @@
 #ifndef classes_h
 #define classes_h
 
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
 typedef long long ll;
 typedef long long T;
 const T nul = NULL;
-const ll c = 12; 
+const ll c = 5; 
+
+void asc_sort(vector<ll>& a)
+{
+    sort(a.begin(), a.end());
+    return;
+}
+
+void desc_sort(vector<ll>& a)
+{
+    sort(a.rbegin(), a.rend());
+    return;
+}
+
+ll median(vector<ll> a)
+{
+    asc_sort(a);
+    int u = a.size();
+    u = (u-1)/2;
+    return a[u];
+}
 
 class bf
 {
@@ -16,8 +39,14 @@ public:
     vector<T> st;
     ll next;
     T pivot;
+    bf()
+    {
+        next = -1;
+        pivot = INT_MIN;
+    }
 };
 
+// make new_lvl function
 class lvl
 {
 public:
@@ -35,13 +64,15 @@ public:
     */
     const ll up_sz, min_sz, down_sz, down_cnt;
     ll cnt;
-    lvl(const ll x) : up_sz (pow(x,1.5)), min_sz(x), down_sz (2*x - 1), down_cnt(ceil(1 + pow(x,0.5)))
+    lvl(const ll x) : up_sz (pow(x,1.5)), min_sz(x), down_sz (2*x - 1), down_cnt(ceil(pow(x,0.5))+1)
     {
         fir = -1;
         down_bf_cnt = 0;
         mex = 0;
-
-        //Add later
+        bf empty_buf;
+        for(int i = 0; i <= down_cnt; i++) down_bf.push_back(empty_buf);
+// cout<<"done"<<endl;
+//Add later
     }
 }; 
 
@@ -50,12 +81,35 @@ class priority_q
 public:
     vector<lvl> level;          //Levels numbered from 0 to level.size()-1
     vector<T> ins_bf, del_bf;   //insert and delete buffers
-    const ll ins_size;                //maximum size of insert and delete buffers
+    const ll ins_size = 5;                //maximum size of insert and delete buffers
+// implement priority_q size
     void insert(T val);         
     T del_min();
     void push(const ll, vector<T>&);
     void pull(const ll, vector<T>&);
     void split(const ll, const ll);
+    void traverse(ll lno)
+    {
+        if(lno >= level.size()) 
+        {
+            cout<<endl;
+            return;
+        }
+        cout<<"TRAVERSE\n";
+        cout<<"down_bf_size "<<level[lno].down_bf_cnt<<" "<<level[lno].min_sz<<" "<<level[lno].down_cnt<<endl;
+        ll i = level[lno].fir;
+        if(i == -1) return;
+        while(true)
+        {
+            cout<<"i is "<<i<<", pivot is "<<level[lno].down_bf[i].pivot<<endl;
+            for(ll x : level[lno].down_bf[i].st)
+                cout<<x<<" ";
+            cout<<endl;
+            if(level[lno].down_bf[i].next == -1) break;
+            i = level[lno].down_bf[i].next;
+        }
+        cout<<endl<<endl;
+    }
 
     void make_newlvl(ll,vector<T>&);
     void push_to_upbf(ll);
