2a79775fa4250d456b0c35bdeb6a7a1651eb2faa
Aditya C <87110942+AdityaC-003@users.noreply.github.com>
2022-02-27 23:57:01 +0530

    Update main.cpp

--- priority_q/main.cpp
+++ priority_q/main.cpp
@@ -2,47 +2,40 @@
 #include "PQ_func.h"
 #include "push.h"
 #include "pull.h"
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
-int main()
+using namespace __gnu_pbds;
+using namespace std;
+
+#define int long long
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int gr(int r)
+{
+   uniform_int_distribution<int> uid(0,r-1);
+   int val = uid(rng);
+   return val+1;
+}
+
+int glr(int l, int r)
+{
+    int dif = r-l+1;
+    int x = gr(dif) + l-1;
+    return x;
+}
+
+int32_t main()
 {
     priority_q a;
-    for(int i = 1; i <= 20; i++) 
-    {
-        //cout<<"\nInserting "<<i<<"...\n";
-        a.insert(i);
-        //a.traverse(0);
-    }
-cout<<"yes"<<endl;
-    for(int i = 1; i <= 10; i++)
-    {
-        cout<<endl<<a.del_min()<<" s"<<endl;
-        // a.traverse(0);
-    }
-
-    for(int i = 30; i <= 37; i++) 
-    {
-        cout<<"\nInserting "<<i<<"...\n";
-        a.insert(i);
-        a.traverse(0);
-        cout<<"\ndel_bf "<<i<<"...\n";
-    for(ll x: a.del_bf)
-    {
-        cout<<x<<" ";
-    }
-    cout<<endl;
-    }
-
-    for(int i = 1; i <= 17; i++)
-    {
-        cout<<a.del_min()<<" s"<<endl;
-    }
-    cout<<endl;
-    // for(int i = 1; i <= 4; i++) cout<<"s "<<a.del_min()<<" ";
-    // cout<<endl;
-    /*for(int i = 30; i >= 20; i--)
-    {
-        a.insert(i);
-    }
-    for(int i = 1; i <= 17; i++) a.del_min();*/
-    return 0;
+    for(int i=0; i<40; i++)
+        a.insert(glr(1,200));
+    
+    a.traverse(0);
+    a.traverse(1);
+    a.traverse(2);
 }
