65ae0c1e90673da9de6ba839c9a0ff9f09f4998e
Aditya C <87110942+AdityaC-003@users.noreply.github.com>
2022-03-06 12:12:59 +0530

    Update PQ_func.h

--- priority_q/PQ_func.h
+++ priority_q/PQ_func.h
@@ -22,22 +22,14 @@ pair<ll,ll> priority_q::last_down_bf(ll lno)
 
 void priority_q::insert(T val)
 {
-    if(del_bf.size() < ins_size)
-    {
-        del_bf.push_back(val);
-        cout<<"Inserted "<<val<<" in ins/del_bf!\n\n";
-        return;
-    }
     del_bf.push_back(val);
-asc_sort(del_bf);
+    asc_sort(del_bf);
     ll push_ins_bf = del_bf.back();
     del_bf.pop_back();
     ins_bf.push_back(push_ins_bf);
     if(ins_bf.size() >= ins_size)
     {
         cout<<"\nInsertion Buffer Full! : ";
-        for(ll x : ins_bf) cout<<x<<' ';
-        cout<<endl;
         cout<<"Pushing to level 0...\n";
         traverse(0);
         push(0, ins_bf);
@@ -47,45 +39,54 @@ asc_sort(del_bf);
     {
         cout<<"Inserted "<<val<<" in ins/del_bf!\n\n";
     }
+    p_b();
 }
 
 T priority_q::del_min()
 {
-cout<<endl<<"ins_bf"<<endl;
-for(ll x : ins_bf)
-    cout<<x<<" ";
-cout<<endl;
-    if(del_bf.size() == 0) return nul;
-desc_sort(del_bf);
-    ll rett = del_bf.back();
-    del_bf.pop_back();
-    if(del_bf.size() == 0)
+    cout<<"Entering del_min...\n\n";
+    p_b();
+    if(del_bf.size())
     {
-        pull(0, del_bf);
-        while(ins_bf.size() > 0)
-        {
-            del_bf.push_back(ins_bf.back());
-            ins_bf.pop_back();
-        }
+        desc_sort(del_bf);
+        ll rett = del_bf.back();
+        del_bf.pop_back();
+        return rett;
     }
-    // T ret = del_bf.back();
-    // del_bf.pop_back();
-asc_sort(del_bf);
-    while(del_bf.size() > ins_size)
+    
+    pull(0,del_bf);
+    ll sizzz = del_bf.size();
+    cout<<sizzz<<endl;
+    while(ins_bf.size() > 0)
+    {
+        del_bf.push_back(ins_bf.back());
+        ins_bf.pop_back();
+    }
+    asc_sort(del_bf);
+    while(sizzz>0 && del_bf.size() > sizzz)
     {
         ins_bf.push_back(del_bf.back());
         del_bf.pop_back();
     }
-cout<<endl<<"del_min  ins_bf"<<endl;
-for(ll x : ins_bf)
-    cout<<x<<" ";
-cout<<endl;
-cout<<"del_bf"<<endl;
-for(ll x : del_bf)
-    cout<<x<<" ";
-cout<<endl;
+
+    desc_sort(del_bf);
+    ll rett = del_bf.back();
+    del_bf.pop_back();
+    cout<<"After pulling:\n";
+    p_b();
+    cout<<"Leaving del_min...\n\n";
     return rett;
 }
 
+void priority_q::p_b()
+{
+    cout<<"Insertion buffer: ";
+    for(ll x : ins_bf)  cout<<x<<' ';
+    cout<<endl;
+    cout<<"Deletion buffer: ";
+    for(ll x : del_bf)  cout<<x<<' ';
+    cout<<endl;
+    return;
+}
 
 #endif
